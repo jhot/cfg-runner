1b65163648204366ded2c83cbf10329c6c319748
John Hotovy <john.hotovy@gmail.com>
2013-11-25 20:25:02 -0600

    Turn off debug :D

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,7 @@ import java.util.Stack;
 
 public class Main
 {
-	private static final boolean DEBUG = true;
+	private static final boolean DEBUG = false;
 	
 	public static void main(String[] args)
 	{
